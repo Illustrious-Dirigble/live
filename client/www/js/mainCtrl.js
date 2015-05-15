ed583d5d07b0df2b541a1efe25408a973a138048
Matt Brown <mattfdbrown@gmail.com>
2015-05-14 17:56:12 -0700

    fix bug

--- client/www/js/mainCtrl.js
+++ client/www/js/mainCtrl.js
@@ -32,7 +32,7 @@ and clears query so the search bar is empty*/
 
   $scope.obj = liveFactory;
 
-  $scope.recordVideo = function(){
+  $scope.Record = function(){
     console.log('record video');
     //angular.element("#captureVideo").trigger('click');
     $("#videoCapture").trigger('click');
